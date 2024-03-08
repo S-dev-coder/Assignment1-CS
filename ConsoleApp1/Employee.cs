3e03261884d11250bda677431e31e5b3494bd8b1
S-dev-coder <sk7137389@gmail.com>
2024-03-08 14:59:35 +0530

    Minor Changes

--- ConsoleApp1/Employee.cs
+++ ConsoleApp1/Employee.cs
@@ -1,56 +1,105 @@
 ﻿using System;
-using System.Collections.Generic;
 
-namespace EmployeeManagementSystem
+
+//Employee class with properties and methods as per the assignment requirements
+public class Employee
 {
-    public class Employee
+    private int Id;
+    private string Name;
+    private string DepartmentName;
+
+    // Define an event to notify when any method is called
+    public event EventHandler<string> MethodCalled;
+
+    // Constructor to initialize the Employee object with provided details
+    public Employee(int id, string name, string departmentName)
     {
-        public delegate void MethodCalledEventHandler(object sender, string message); // Event delegate for notification
-
-        private int id;
-        private string name;
-        private string departmentName;
-
-        public event MethodCalledEventHandler MethodCalled; // Event declaration
-
-        public Employee(int id, string name, string departmentName)
-        {
-            this.id = id;
-            this.name = name;
-            this.departmentName = departmentName;
-        }
-
-        public int GetId()
-        {
-            return id;
-        }
-
-        public void UpdateId(int newId)
-        {
-            id = newId;
-            MethodCalled?.Invoke(this, $"Employee ID updated to {newId}"); // Raise event after update
-        }
-
-        public string GetName()
-        {
-            return name;
-        }
-
-        public void UpdateName(string newName)
-        {
-            name = newName;
-            MethodCalled?.Invoke(this, $"Employee name updated to {newName}"); // Raise event after update
-        }
-
-        public string GetDepartmentName()
-        {
-            return departmentName;
-        }
-
-        public void UpdateDepartmentName(string newDepartmentName)
-        {
-            departmentName = newDepartmentName;
-            MethodCalled?.Invoke(this, $"Employee department name updated to {newDepartmentName}"); // Raise event after update
-        }
+        Id = id;
+        Name = name;
+        DepartmentName = departmentName;
+    }
+    // Method to return the Id property
+    public int GetId()
+    {
+        OnMethodCalled("GetId() method called");
+        return Id;
+    }
+    // Method to return the Name property
+    public string GetName()
+    {
+        OnMethodCalled("GetName() method called");
+        return Name;
+    }
+    // Method to return the DepartmentName property
+    public string GetDepartmentName()
+    {
+        OnMethodCalled("GetDepartmentName() method called");
+        return DepartmentName;
+    }
+    // Method to update the Id property
+    public void UpdateId(int newId)
+    {
+        Id = newId;
+    }
+    // Method to update the Name property
+    public void UpdateName(string newName)
+    {
+        Name = newName;
+    }
+    // Method to update the DepartmentName property
+    public void UpdateDepartmentName(string newDepartmentName)
+    {
+        DepartmentName = newDepartmentName;
+    }
+    // Event handler to raise an event when any method is called
+    protected virtual void OnMethodCalled(string message)
+    {
+        MethodCalled?.Invoke(this, message);
     }
 }
+class Program
+{
+    static void Main(string[] args)
+    {
+        //  To enter employee details
+        Console.WriteLine("Enter Employee Details:");
+        Console.Write("Id: ");
+        int id = int.Parse(Console.ReadLine());
+        Console.Write("Name: ");
+        string name = Console.ReadLine();
+        Console.Write("Department Name: ");
+        string departmentName = Console.ReadLine();
+
+
+        // Create an instance of the Employee class with provided details
+        Employee emp = new Employee(id, name, departmentName);
+
+
+     
+        emp.MethodCalled += (sender, message) => Console.WriteLine(message);
+
+
+        // Display the employee details using the Get methods
+        Console.WriteLine("\nEmployee Details:");
+        Console.WriteLine("Id: " + emp.GetId());
+        Console.WriteLine("Name: " + emp.GetName());
+        Console.WriteLine("Department Name: " + emp.GetDepartmentName());
+
+
+        // To update employee details
+        Console.WriteLine("\nEnter Updated Details:");
+        Console.Write("Id: ");
+        emp.UpdateId(int.Parse(Console.ReadLine()));
+        Console.Write("Name: ");
+        emp.UpdateName(Console.ReadLine());
+        Console.Write("Department Name: ");
+        emp.UpdateDepartmentName(Console.ReadLine());
+
+        // Display the updated employee details
+        Console.WriteLine("\nUpdated Employee Details:");
+        Console.WriteLine("Id: " + emp.GetId());
+        Console.WriteLine("Name: " + emp.GetName());
+        Console.WriteLine("Department Name: " + emp.GetDepartmentName());
+    }
+}
+
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df4c0d36b26bac6dd83bc40565dfad112d0f7ecc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
